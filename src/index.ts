c8431c67bb14a8c931ebcf8966f7d2a66c6190b2
christiano.bovo <christiano.bovo@sonda.com>
2019-09-12 18:33:59 +0000

    Setup Package

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,45 @@
 import commander from 'commander';
 import { RBVM } from './rbvm';
 
-let packageJson;
-try {
-  // tslint:disable-next-line:no-var-requires
-  packageJson = require('./package.json');
-} catch {
-  // tslint:disable-next-line:no-var-requires
-  packageJson = require('../package.json');
-}
-
-commander.name('rbvm');
-commander.usage('[options] <oldFile> <newFile> <outputMergedFile>');
-commander.version(packageJson.version, '-v, --version', 'output the current version');
-commander.arguments('rbvm <oldFile> <newFile> <outputMergedFile>');
-commander.action((oldFile: any, newFile: any, outputMergedFile: any) => {
-  // console.log('rbvm oldFile: %s newFile: %s outputMergedFile: %s', oldFile, newFile, outputMergedFile);
-
-  RBVM.merge(oldFile, newFile, outputMergedFile, (error, result) => {
-    if (error) {
-      console.error('Merged Error ->', error);
-    } else {
-      console.info('Merged Completed!');
-    }
-  });
-});
-
-commander.on('--help', () => {
+let helpMessage = () => {
   console.log('');
   console.log('Example:');
   console.log('  $ rbvm language.pt-BR.v1.js language.pt-BR.v2.js language.pt-BR.v3.js');
-});
+};
 
-commander.parse(process.argv);
+var myArgs = process.argv.slice(2);
+if (myArgs.length === 0) {
+  helpMessage();
+} else {
+  // check app version
+  let packageJson;
+  try {
+    // tslint:disable-next-line:no-var-requires
+    packageJson = require('./package.json');
+  } catch {
+    // tslint:disable-next-line:no-var-requires
+    packageJson = require('../package.json');
+  }
+
+  commander.name('rbvm');
+  commander.usage('[options] <oldFile> <newFile> <outputMergedFile>');
+  commander.version(packageJson.version, '-v, --version', 'output the current version');
+  commander.arguments('rbvm <oldFile> <newFile> <outputMergedFile>');
+  commander.action((oldFile: any, newFile: any, outputMergedFile: any) => {
+    // console.log('rbvm oldFile: %s newFile: %s outputMergedFile: %s', oldFile, newFile, outputMergedFile);
+
+    RBVM.merge(oldFile, newFile, outputMergedFile, (error, result) => {
+      if (error) {
+        console.error('Merged Error ->', error);
+      } else {
+        console.info('Merged Completed!');
+      }
+    });
+  });
+
+  commander.on('--help', () => {
+    helpMessage();
+  });
+
+  commander.parse(process.argv);
+}
